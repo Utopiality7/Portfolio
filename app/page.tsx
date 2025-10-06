ea21dd8839e73e5a8e16e77554cd21713491890c
utopiality7 <akirameruna7315@gmail.com>
2025-10-05 20:16:12 -0400

    start with about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
-import type { NextPage } from "next"
-import Head from "next/head"
-import Background from "../components/Background"
-import Menus from "../components/Menus"
-import ProfileCard from "../components/ProfileCard"
+import type { NextPage } from "next";
+import Head from "next/head";
+import About from "../components/About";
+import Background from "../components/Background";
+import Menus from "../components/Menus";
+import ProfileCard from "../components/ProfileCard";
 
 const Home: NextPage = () => {
   return (
     <main className="min-h-screen relative home flex justify-center items-center">
       <Background />
-      <section className="z-10 w-[126.8rem] h-[62.5rem] flex gap-4">
+      <section className="z-10 w-[126.8rem] h-[62.5rem] flex">
         <Menus />
         <ProfileCard />
+
+        <div className="w-[71.6rem] h-full py-6">
+          <About />
+        </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
