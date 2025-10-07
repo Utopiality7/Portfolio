ba21ca9d905cd9767d0a557cdfa28c092efffcb7
utopiality7 <akirameruna7315@gmail.com>
2025-10-07 19:51:58 -0400

    done with about page

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import Testimonial from "./Testimonial";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
 export default function Testimonials() {
-  return <div>Testimonials</div>;
+  return (
+    <div className="testimonials">
+      <Swiper pagination={true} modules={[Pagination]}>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Testimonial />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
