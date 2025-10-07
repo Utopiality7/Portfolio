0b9fc50d1e23e773a3ceaea2bf54dfe154c590b4
utopiality7 <akirameruna7315@gmail.com>
2025-10-07 08:09:17 -0400

    done with fun facts part

---
+++ components/Facts.tsx
@@ -0,0 +1,21 @@
+import { FaReact } from "react-icons/fa";
+import Fact from "./Fact";
+
+export default function Facts() {
+  return (
+    <ul className="logos grid grid-cols-4 mb-20">
+      <li>
+        <Fact Icon={FaReact} desc="500+ Movies Watched" border />
+      </li>
+      <li>
+        <Fact Icon={FaReact} desc="30+ New Recipes Tried" border />
+      </li>
+      <li>
+        <Fact Icon={FaReact} desc="140+ Sunsets Witnessed" border />
+      </li>
+      <li>
+        <Fact Icon={FaReact} desc="320+ Books Read" border />
+      </li>
+    </ul>
+  );
+}
